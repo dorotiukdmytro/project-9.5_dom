11fc1c3f0a79016b3d27c533b194b891de74abe8
Dmytro <ddorotiuk@gmail.com>
2017-07-17 13:49:50 +0200

    Add html structure

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts
+
+var buttonClassNames = document.getElementsByClassName("button");
+for (i = 0 ; i < buttonClassNames.length ; i++) {
+	alert(buttonClassNames.innerText = "Hello!");
+}
+
+
